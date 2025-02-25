27c5d361c9a5549f2b77f4b0ba24718966542118
SHIN SUJUNG <shinssjj79@gmail.com>
2025-02-25 21:15:27 +0900

    Initial commit

---
+++ src/main/java/com/example/mini_farm_game/MiniFarmGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_farm_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFarmGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniFarmGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_farm_game/MiniFarmGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_farm_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFarmGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
